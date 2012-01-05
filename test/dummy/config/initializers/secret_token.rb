62bae2a97bf2b1f83f5cee012b8fbb258d670bab
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-01-06 05:23:40 +0900

    removed test files

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'df920e2a4c6d13b50ad08e5c4ecad0acc4c5925145b869a79113587c204b90059eeb79270bce426b9448733f2faa24772dddf68ee636ade6f444a66847407032'
--- test/enju_barcode_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class EnjuBarcodeTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, EnjuBarcode
-  end
-end
